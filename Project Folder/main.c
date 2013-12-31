c301977a302199a00ae4f6f185604bca76f16204
Mechtecs <mechtecs@gmail.com>
2013-12-31 17:40:44 +0100

    Creating SGC.BSP

--- Project Folder/main.c
+++
@@ -1,8 +0,0 @@
-ENTITY* SGC;
-function main(){
-	level_load("small.hmp");
-	SGC = ent_create("sgc.mdl",vector(0,0,0),NULL);
-	SGC.scale_x=10;
-	SGC.scale_y=10;
-	SGC.scale_z=10;
-}
--- Source/run_mod.bat
+++
@@ -1,1 +0,0 @@
-@"C:\Program Files (x86)\Steam\steamapps\common\source sdk base 2007\hl2.exe" -dev -game "C:\Program Files (x86)\Steam\steamapps\SourceMods\stargateinfinity" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
